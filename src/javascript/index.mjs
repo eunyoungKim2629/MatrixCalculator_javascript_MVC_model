d3b70014a10e2059d4bbbed14c60478e61648483
KIMEUNYOUNG <kimeunyoung@tests-MacBook-Pro.local>
2022-05-21 21:22:33 +0900

    index.pug, style.scss, Controller.js, index.mjs, ElementTool.js 수정 완료, NomalMatrixContainer.js 파일 생성 및 작성 완료

--- src/javascript/index.mjs
+++ src/javascript/index.mjs
@@ -1,3 +1,3 @@
-import Controller from './controller/controller'; 
+import Controller from './controller/controller.js'; 
 
 window.addEventListener('DOMContentLoaded', () => void new Controller());
--- src/javascript/utils/ElementTool.js
+++ src/javascript/utils/ElementTool.js
@@ -4,7 +4,7 @@ export const $$ = (elements) => document.querySelectorAll(elements);
 
 export const createElement = (tagName, tagText = '') => {
 	const element = document.createElement(tagName);
-	element.InnerText = tagTex;
+	element.textContent = tagText;
 
 	return element;
 };
@@ -12,7 +12,8 @@ export const createElement = (tagName, tagText = '') => {
 export const combineElement = (elements) => {
 	const fragment = document.createDocumentFragment();
 	elements.forEach((element) => fragment.append(element));
-	return fragment;
+	
+  return fragment;
 };
 
 export const removeElement = (parentSelector, selfSelector) => void document.querySelector(parentSelector).querySelector(selfSelector).remove();
