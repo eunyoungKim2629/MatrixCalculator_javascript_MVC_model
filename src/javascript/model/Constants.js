6a8e196255d57f90068fbba210b191942205c69c
eunyoungKim2629 <xjoax7@gmail.com>
2022-05-28 11:52:46 +0900

    Constans.js  수정 완료

--- src/javascript/model/Constants.js
+++ src/javascript/model/Constants.js
@@ -1,25 +1,26 @@
 export default Object.freeze({
-  GENERAL_MATRIX: {
-    ROW_KEYWORD: 'ROW',
-    COL_KEYWORD: 'COL',
-    CREATE_KEYWORD: 'CREATE',
-    FIRST_MATRIX_TITLE: 'First Matrix',
-    SECOND_MATRIX_TITLE: 'Second Matrix'
-  },
-  CALC_MATRIX: {
-    CALC_MATRIX_TITLE: 'Matrix Calculator',
-    AUTHOR: ['Eunyoung Kim', 'Yejeung Ji']
-  },
-  WANING_KEYWORD: {
-    WARNING01: '0~9 까지의 숫자만을 입력해 주세요.',
-    WARNING02: '-99 ~99 까지의 숫자만을 입력해 주세요',
-    WARNING03: '행렬의 덧셈을 위해서는 반드시 두 행렬의 행과 열의 개수가 같아야 합니다.',
-    WARNING04: '행렬의 뺄셈을 위해서는 반드시 두 행렬의 행과 열의 개수가 같아야 합니다.',
-    WARNING05: '행렬의 곱셈을 위해서는 반드시 첫 번째 행렬의 행의 개수와 두 번째 행렬의 열의 개수가 같아야 합니다.',
-    WARNING06: '첫 번째 행렬을 완성하여 주세요',
-    WARNING07: '두 번째 행렬을 완성하여 주세요', 
-    WARNING08: '연산을 위하여 두 행렬을 완성하여 주세요',
-    WARNING09: '첫 번째 행렬의 값을 확인하여 주세요',
-    WARNING10: '두 번째 행렬의 값을 확인하여 주세요',
-  },
+	GENERAL_MATRIX: {
+		ROW_KEYWORD: 'ROW',
+		COL_KEYWORD: 'COL',
+		CREATE_KEYWORD: 'CREATE',
+		FIRST_MATRIX_TITLE: 'First Matrix',
+		SECOND_MATRIX_TITLE: 'Second Matrix',
+	},
+	CALC_MATRIX: {
+		CALC_MATRIX_TITLE: 'Matrix Calculator',
+		AUTHOR: ['Yejeung Ji', 'Eunyoung Kim'],
+	},
+	WARNING_KEYWORD: {
+		WARNING01: '공란이 존재합니다.',
+		WARNING02: '0 ~ 9 까지의 숫자를 입력해 주세요.',
+		WARNING03: '-99 ~ 99 까지의 숫자만을 입력해 주세요.',
+		WARNING04: '행렬의 덧셈을 위해서는 반드시 두 행렬의 행과 열의 개수가 같아야합니다.',
+		WARNING05: '행렬의 뺄셈을 위해서는 반드시 두 행렬의 행과 열의 개수가 같아야합니다.',
+		WARNING06: '행렬의 곱셈을 위해서는 반드시 첫 번째 행렬의 행의 개수와 두 번째 행렬의 열의 개수가 같아야합니다.',
+		WARNING07: '첫 번째 행렬을 완성하여 주세요.',
+		WARNING08: '두 번째 행렬을 완성하여 주세요.',
+		WARNING09: '연산을 위하여 두 행렬을 완성하여 주세요.',
+		WARNING10: '첫 번째 행렬의 값을 확인하여 주세요.',
+		WARNING11: '두 번째 행렬의 값을 확인하여 주세요.',
+	},
 });
